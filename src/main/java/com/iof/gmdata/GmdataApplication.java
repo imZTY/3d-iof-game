e0f7f7b636ccf384abb8d6d958667e16e9eb6da3
18320444515 <32980506+18320444515@users.noreply.github.com>
2019-01-23 15:09:07 +0800

    Initial commit

---
+++ src/main/java/com/iof/gmdata/GmdataApplication.java
@@ -0,0 +1,14 @@
+package com.iof.gmdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdataApplication.class, args);
+	}
+
+}
+
